0d0bf016d03075e570c0afcc01bcbb13401e4b59
Etten32 <etten327vcr@gmail.com>
2021-09-24 16:02:42 -0400

    Pseudo-coded loading steps

--- src/MainDriver.java
+++ src/MainDriver.java
@@ -5,3 +5,17 @@ public class MainDriver{
 	
 	}
 }
+// start up screen
+// check if allGames.txt is empty (for load game button activation)
+// load allMissions[] {
+//	// load in data/name <-from- allMissions.txt
+//	// set number of missions int
+//	// set allMissions[] size = number of missions
+//	// get roomsString[] from .msn file
+//	// for each room get info from .room files
+//	// get all door info from .dr files
+//	// initialize all doors in a new door[]
+//	// initialize all rooms in a new room[] (must get a list of DOuts in order to do this)
+//	// initialize/create mission in allMissions[] <-send- door[] and room[]
+//	}
+// Enable main menu
